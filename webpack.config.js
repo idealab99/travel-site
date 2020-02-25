f60814356a906c79062a3b512f3d2efbda93fc35
Harshad <Idea.lab99@gmail.com>
2020-02-25 07:05:34 +0000

    completed hero and content

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function (app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        open: true,
+        host: '0.0.0.0',
+        port: 8181,
+        useLocalIp: true,
+        
+
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
